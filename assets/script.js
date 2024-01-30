c363d67bb7468840bc8082b2d2c70b5ea662cfb5
Tamerbekir <astoryofame@gmail.com>
2024-01-30 00:33:20 -0500

    added works and receipes generated sections on HTML. Removed javascript function that called API. Was not working as inteded and was not being pulled from API and appended on to page. Wrote comments

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,43 @@
-// Exercises API k163haKKqYGpamQeCQMW4A==hVWwjQzS9u8h36xK //tamer 
+//created variables for URL and key
+const workoutUrl = "https://api.api-ninjas.com/v1/caloriesburned?activity="
+const workoutApi = "k163haKKqYGpamQeCQMW4A==hVWwjQzS9u8h36xK"
 
 
+document.getElementById("workoutBtn").addEventListener("click", function () {
+    function getExercises() {
+        const activity = "running"
+        $.ajax({
+            method: 'GET',
+            url: workoutUrl + activity,
+            headers: { 'X-Api-Key': workoutApi },
+            contentType: 'application/json',
+            success: function (result) {
+                console.log(result);
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
+            
+        });
+        
+    }
+    getExercises()
+    //to show that when the button is clicked, api is working
+    console.log("button click to pull API working")
+});
 
-function exercises() {
-    var activity = 'walking'
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/caloriesburned?activity=' + activity,
-        headers: { 'X-Api-Key': 'k163haKKqYGpamQeCQMW4A==hVWwjQzS9u8h36xK'},
-        contentType: 'application/json',
-        success: function(result) {
-            console.log(result);
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
+//! currently not working
+function exercises(workoutData) {
+    const {calories_per_hour} = workoutData
+    if (workoutData) {
+        //created element for the date
+        const calories = calories_per_hour
+        const caloriesTitle = document.createElement('p')
+        caloriesTitle.textContent = `${calories}`
+        const caloriesSection = document.getElementById('workoutName')
+        caloriesSection.innerHTML = ''
+        caloriesSection.appendChild(caloriesTitle)
+    }
 }
 
-let activity = "running"
-
-function workout(workoutData) {
-    const {name, calories_per_hour} = workoutData;
-  
-    if (workoutData) {
-      const nameTitle = document.createElement('h6');
-      nameTitle.textContent = `Workout Name: ${activity}, Calories Per Hour: ${calories_per_hour}`
-      
-      const nameSection = document.getElementById('name')
-      nameSection.innerHTML = ''
-      nameSection.appendChild(nameTitle);
-      console.log(workoutData)
-    }
-  }
-  
-  document.getElementById('button').addEventListener('click', function() {
-    const sampleWorkoutData = {
-      name: 'activity',
-      calories_per_hour: 500
-    }
-    workout(sampleWorkoutData);
-  })
-  exercises()
+document.getElementById('workoutBtn').addEventListener('click', exercises())
