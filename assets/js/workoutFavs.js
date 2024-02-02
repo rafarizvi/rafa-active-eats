cf84fc5e9196c7c6af758ec20489f791e8bfbe56
Tamerbekir <astoryofame@gmail.com>
2024-02-01 21:30:55 -0500

    accepeted current changes to HTML file

--- assets/js/workoutFavs.js
+++ assets/js/workoutFavs.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 let storedWorkout = localStorage.getItem("storedWorkout");
-=======
-let allWorkouts = localStorage.getItem("allWorkouts");
->>>>>>> 6a9abbcc8cad7b37608dfe4602a24ef65bd6c78c
 let newFav = document.getElementById("allFavorites");
 let clear = document.querySelector("#clear");
 
@@ -14,7 +10,6 @@ clear.addEventListener("click", function () {
 goBack.addEventListener("click", function () {
     window.location.replace("./index.html");
 });
-<<<<<<< HEAD
 storedWorkout = JSON.parse(storedWorkout);
 
 if (storedWorkout !== null) {
@@ -24,17 +19,6 @@ if (storedWorkout !== null) {
         let workoutFavSection = document.createElement("li");
         //workout and activity current undefinded
         workoutFavSection.textContent = storedWorkout[i].Workout + " : " + storedWorkout[i].Activity;
-=======
-allWorkouts = JSON.parse(allWorkouts);
-
-if (allWorkouts !== null) {
-
-    for (let i = 0; i < allWorkouts.length; i++) {
-
-        let workoutFavSection = document.createElement("li");
-        //workout and activity current undefinded
-        workoutFavSection.textContent = allWorkouts[i].workout + " : " + allWorkouts[i].activity;
->>>>>>> 6a9abbcc8cad7b37608dfe4602a24ef65bd6c78c
         newFav.appendChild(workoutFavSection)
 
     }
